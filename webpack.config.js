f01b73695da193cf4da091a70e61f0542fcd2bcb
Dnyaneshwar Bhalerao <dnyaneshwar.bhalerao@synerzip.com>
2017-08-22 14:37:55 +0530

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/client/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  devtool: 'source-map',
+  entry: __dirname + '/client/src/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader:'babel-loader'
+      }
+    ]
+  },
+  output:{
+    filename:'bundle.js',
+    path:__dirname+'/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
