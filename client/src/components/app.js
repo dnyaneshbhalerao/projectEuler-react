5f68cc7422bd7fb8b16b72d437b12a67f85b0290
Dnyaneshwar Bhalerao <dnyaneshwar.bhalerao@synerzip.com>
2017-10-11 12:48:24 +0530

    Added a header component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './common/header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -7,7 +8,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>Hello from react</div>
+      <Header/>
     );
   }
 }
